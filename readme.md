419ffe8f8595c39061fd0df7fa0a2b270b82ca5e
Devan Buggay <sbuggay@gmail.com>
2018-02-12 22:51:36 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+
+
+```
+type	"I"
+version	17
+serverName	"LinuxGSM"
+map	"de_mirage"
+gameType	"csgo"
+gameName	"Counter-Strike: Global Offensive"
+appID	730
+numPlayers	0
+maxPlayers	16
+numBots	0
+dedicated	"dedicated"
+os	"Linux"
+password	0
+secure	true
+gameVersion	"1.36.2.6"
+ip	"192.168.1.3"
+port	27015
+pw	false
+```
---
+++ src/main.js
@@ -0,0 +1,40 @@
+const restify = require("restify");
+const srcds = require("srcds-info");
+
+const server = restify.createServer({
+    name: "rcon-proxy",
+    version: '1.0.0'
+});
+
+function getStatus(address, port = 27015) {
+    return new Promise((resolve, reject) => {
+        const client = srcds(address, port);
+        client.info((err, info) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(info);
+            client.close();
+        });
+    });
+}
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get("/", (req, res, next) => {
+    if (!req.query.ip) {
+        res.send(500);
+    }
+
+    getStatus(req.query.ip, req.query.port).then((result) => {
+        res.send(result);
+    });
+    
+    return next();
+});
+
+server.listen(8080, () => {
+    console.log("%s listening at %s", server.name, server.url);
+});
